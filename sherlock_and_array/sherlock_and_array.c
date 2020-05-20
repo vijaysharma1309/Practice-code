dca2bdd753d1afc9c10e002f47617641759a4599
vijaysharma1309 <vj.s1309@gmail.com>
2020-05-20 16:52:29 +0530

    made a function

--- sherlock_and_array/sherlock_and_array.c
+++ sherlock_and_array/sherlock_and_array.c
@@ -1,5 +1,39 @@
 #include<stdio.h>
 
+char *balancedSum(int *arr, int n)
+{
+    int i, sum_l = 0, sum_r = 0, s = 0, flag = 0;
+    int sum = 0;
+    
+    for(i = 0; i < n; ++i)
+    {
+        sum += arr[i];
+    }
+    
+    for(i = 0; i < n; ++i)
+    {
+        s += arr[i];
+        sum_r = sum - s;
+        if(i >= 1)
+        {
+            sum_l += arr[i-1];
+        }
+        if(sum_l == sum_r)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 1)
+    {
+        return "YES";
+    }
+    else
+    {
+        return "NO";
+    }
+}
+
 int main()
 {
     int i, t, n;
@@ -9,37 +43,11 @@ int main()
     while(t--)
     {
         scanf("%d" ,&n);
-        int sum = 0, flag = 0;
         int arr[n];
         for(i = 0; i < n; ++i)
         {
             scanf("%d", &arr[i]);
-            sum += arr[i];
         }
-        
-        int sum_l = 0, sum_r = 0, s = 0;
-        
-        for(i = 0; i < n; ++i)
-        {
-            s += arr[i];
-            sum_r = sum - s;
-            if(i >= 1)
-            {
-                sum_l += arr[i-1];
-            }
-            if(sum_l == sum_r)
-            {
-                flag = 1;
-                break;
-            }
-        }
-        if(flag == 1)
-        {
-            printf("YES\n");
-        }
-        else
-        {
-            printf("NO\n");
-        }
+        printf("%s\n", balancedSum(arr, n));  
     }
 }
